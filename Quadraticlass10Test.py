1d4fda01d7413589a3229dfcd235f0f5c96b37ba
Dean Hunter <jeeka7@gmail.com>
2022-06-23 11:38:59 +0530

    Update Quadraticlass10Test.py

--- Quadraticlass10Test.py
+++ Quadraticlass10Test.py
@@ -26,6 +26,8 @@ if selected == 'Quadratic Equations Test':
   def addscore():
     global score
     score = score + 3
+  def DoNothing():
+    pass
   def subtractscore():
     global score
     score = score - 1
@@ -34,18 +36,21 @@ if selected == 'Quadratic Equations Test':
   st.write("Attempt all questions , You get + 3 marks for correct answer , -1 marks for wrong answer and 0 for not attempting")
   
   st.write("Q1 : The Polynomial Equation x (x + 1) + 8 = (x + 2) (x - 2) is")
-  a1=st.radio("Ans.1",('cubic','quadratic','linear','biquadratic'),horizontal=True)
+  a1=st.radio("Ans.1",('cubic','quadratic','linear','biquadratic','SKIP'),horizontal=True)
   if a1 == 'linear':
     addscore()
   elif (a1 == 'cubic')or (a1 =='quadratic')or (a1=='biquadratic'):
     subtractscore()
-  
+  elif (a1 == 'SKIP'):
+    DoNothing()
   st.write("Q2 : The Quadratic equation whore one rational root is 3+\u221A2 is")
-  a2=st.radio("Ans.2",('x\u00b2-7x+5=0','x\u00b2+7x+6=0','x\u00b2-7x+6=0','x\u00b2-6x+7=0'),horizontal=True)
+  a2=st.radio("Ans.2",('x\u00b2-7x+5=0','x\u00b2+7x+6=0','x\u00b2-7x+6=0','x\u00b2-6x+7=0','SKIP'),horizontal=True)
   if a2 == 'x\u00b2+7x+6=0':
     addscore()
   elif (a2 == 'x\u00b2-7x+5=0')or(a1 =='x\u00b2-7x+6=0')or(a1=='x\u00b2-6x+7=0'):
     subtractscore()
+  elif (a2 == 'SKIP'):
+    DoNothing()
   st.write("your score is",score)
 if selected == 'Result':
   st.title(f"You have chosen {selected}")
