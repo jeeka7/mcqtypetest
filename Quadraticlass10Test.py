3f47e660f2155ac7ff042a09df88d13863dd2643
Dean Hunter <jeeka7@gmail.com>
2022-06-23 17:58:35 +0530

    Update Quadraticlass10Test.py

--- Quadraticlass10Test.py
+++ Quadraticlass10Test.py
@@ -71,7 +71,30 @@ INSERT INTO mytable VALUES ('Mary', 3), ('John', 14), ('Robert', 11);
     
 st.write("your final score is",score)
 
+import streamlit as st
+import mysql.connector
 
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
 
 if selected == 'Result':
   st.session_state.result = score
