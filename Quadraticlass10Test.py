b0f6063a18157c30dd02cc75a1775c1905a58327
Dean Hunter <jeeka7@gmail.com>
2022-07-04 06:38:38 +0530

    Update Quadraticlass10Test.py

--- Quadraticlass10Test.py
+++ Quadraticlass10Test.py
@@ -60,32 +60,9 @@ if selected == 'Quadratic Equations Test':
     DoNothing()
 
 
-INSERT INTO mytable VALUES ('Mary', 3), ('John', 14), ('Robert', 11);    
     
 st.write("your final score is",score)
 
-
-# Initialize connection.
-# Uses st.experimental_singleton to only run once.
-@st.experimental_singleton
-def init_connection():
-    return mysql.connector.connect(**st.secrets["mysql"])
-
-conn = init_connection()
-
-# Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
-def run_query(query):
-    with conn.cursor() as cur:
-        cur.execute(query)
-        return cur.fetchall()
-
-rows = run_query("SELECT * from mytable;")
-
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
 
 if selected == 'Result':
   st.session_state.result = score
