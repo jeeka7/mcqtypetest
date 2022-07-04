71be9c6527b109f7441475504749ccd3a7251bd2
Dean Hunter <jeeka7@gmail.com>
2022-07-04 07:20:51 +0530

    Update Quadraticlass10Test.py

--- Quadraticlass10Test.py
+++ Quadraticlass10Test.py
@@ -2,6 +2,31 @@ import streamlit as st
 from streamlit_option_menu import option_menu
 import pandas as pd
 
+import mysql.connector
+
+# Initialize connection.
+# Uses st.experimental_singleton to only run once.
+@st.experimental_singleton
+def init_connection():
+    return mysql.connector.connect(**st.secrets["mysql"])
+
+conn = init_connection()
+
+# Perform query.
+# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+@st.experimental_memo(ttl=600)
+def run_query(query):
+    with conn.cursor() as cur:
+        cur.execute(query)
+        return cur.fetchall()
+
+rows = run_query("SELECT * from mytable;")
+
+# Print results.
+for row in rows:
+    st.write(f"{row[0]} has a :{row[1]}:")
+
+
 menu=['Donate Money','Quadratic Equations Test','Result']
 
 if 'result' not in st.session_state:
